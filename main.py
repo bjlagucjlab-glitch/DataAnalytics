60677b712c5daa49138d1f88ca414f51a250c401
bjlagucjlab-glitch <prodigy.violinistt@gmail.com>
2026-06-18 20:41:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-HERE = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(HERE, 'online_store.db')
-
 def get_connection():
     if not os.path.exists(DB_PATH):
         raise SystemExit(f"Database not found {DB_PATH} ")
