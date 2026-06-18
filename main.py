b063de64c864aa36f6bd0575cad4d8a72573f8f2
bjlagucjlab-glitch <prodigy.violinistt@gmail.com>
2026-06-18 20:33:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import os
+import sqlite3
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(HERE, 'online_store.db')
+
+def get_connection():
+    if not os.path.exists(DB_PATH):
+        raise SystemExit(f"Database not found {DB_PATH} ")
+
+    return sqlite3.connect(DB_PATH)
 
